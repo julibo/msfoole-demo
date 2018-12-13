26bcd8d4f75fed16400e6f32851bb3ecc32b5e0a
carson <yuzhanwei@aliyun.com>
2018-12-13 08:51:48 +0800

    版本更新1212

--- app/Controller/Index/Sale.php
+++ app/Controller/Index/Sale.php
@@ -1,9 +1,13 @@
 <?php
+/**
+ * 官网预约挂号
+ */
+
 namespace App\Controller\Index;
 
 use Julibo\Msfoole\HttpController as BaseController;
 
-class Index extends BaseController
+class Sale extends BaseController
 {
     protected function init()
     {
@@ -13,16 +17,6 @@ class Index extends BaseController
     public function index()
     {
         return 'hello world!';
-    }
-
-    public function login()
-    {
-        $this->setToken(['name'=>'carson', 'age'=>30]);
-    }
-
-    public function getUser()
-    {
-        return $this->getUserByToken();
     }
 
 }
